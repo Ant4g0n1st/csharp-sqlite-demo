9310a481ed3adfe51b9a398ab3198c1f4721ff97
Víctor Noriega <vic.noriega.am@outlook.com>
2021-11-01 00:41:07 -0600

    Random fun with events.

--- SQLiteDemo/SQLiteDemo.View/MainWindow.xaml.cs
+++ SQLiteDemo/SQLiteDemo.View/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
+using SQLiteDemo.Model.User;
 using SQLiteDemo.ViewModel.MainWindow;
 using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SQLiteDemo.View
 {
@@ -25,6 +28,25 @@ namespace SQLiteDemo.View
             base.OnInitialized(e);
             logger.LogInformation("Fetching User List...");
             UserList.ItemsSource = await viewModel.GetAllUsers();
+        }
+
+        private void Border_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Border border = e.Source as Border;
+            if (border != null)
+            {
+                border.Background = Brushes.LightCyan;
+            }
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Border border = e.Source as Border;
+            if (border != null)
+            {
+                IUserModel user = border.DataContext as IUserModel;
+                MessageBox.Show($"You selected user {user.FullName}");
+            }
         }
     }
 }
